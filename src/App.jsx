75d5c01503cd4a95986c3549f7a7f27353926ca0
milanrai <milanraiyt07@gmail.com>
2024-05-13 15:24:04 +0545

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactIndex from './component/ContactIndex'
+
+const App = () => {
+  return (
+    <>
+    <ContactIndex/>
+    </>
+  )
+}
+
+export default App
